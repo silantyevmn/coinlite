63247ac944fc30f40e3817964d5a14276970b605
silantyevmn <5992033@mail.ru>
2021-01-15 19:09:37 +0300

    Правка PaperCache

--- app/src/main/java/silantyevmn/ru/coinlite/mvp/model/cache/paper/PaperCache.kt
+++ app/src/main/java/silantyevmn/ru/coinlite/mvp/model/cache/paper/PaperCache.kt
@@ -7,30 +7,31 @@ import silantyevmn.ru.coinlite.mvp.model.cache.Cache
 import silantyevmn.ru.coinlite.mvp.model.entity.rest.GeckoCoinChartRest
 import silantyevmn.ru.coinlite.mvp.model.entity.rest.GeckoCoinRest
 import java.lang.Exception
-import java.util.*
+import java.lang.RuntimeException
 
-class PaperCache(): Cache{
-    val BASE_KEY = "CoinMarket"
-    val KEY_CHART = "Chart"
+class PaperCache : Cache {
+    private val BASE_KEY = "CoinMarket"
+    private val KEY_CHART = "Chart"
     override fun getCoinMarket(): Observable<List<GeckoCoinRest>> {
-        return Observable.create{ emitter: ObservableEmitter<List<GeckoCoinRest>> ->
-            var list : List<GeckoCoinRest> = emptyList()
+        return Observable.create { emitter: ObservableEmitter<List<GeckoCoinRest>> ->
             try {
-                list = Paper.book().read(BASE_KEY);
-            } catch (e:Exception){
-                //нет базы вернем emptyList
+                val list: List<GeckoCoinRest> = Paper.book().read(BASE_KEY);
+                emitter.onNext(list)
+            } catch (e: Exception) {
+                emitter.onError(RuntimeException("Cache CoinMarket is null"))
             }
-            emitter.onNext(list)
             emitter.onComplete()
         }
     }
 
     override fun putAllCoinMarket(list: List<GeckoCoinRest>): Boolean {
         try {
-            Paper.book().delete(BASE_KEY)
+            if (Paper.book().contains(BASE_KEY)) {
+                Paper.book().delete(BASE_KEY)
+            }
             Paper.book().write(BASE_KEY, list)
             return true
-        } catch (e: Exception){
+        } catch (e: Exception) {
             return false
         }
     }
@@ -39,21 +40,23 @@ class PaperCache(): Cache{
         return Observable.create { emitter: ObservableEmitter<GeckoCoinChartRest> ->
             try {
                 val item: GeckoCoinChartRest = Paper.book(KEY_CHART).read(id)
-                emitter.onNext(item);
+                emitter.onNext(item)
             } catch (e: Exception) {
-                //нет базы вернем пустой объект
-                emitter.onNext(GeckoCoinChartRest(listOf()))
+                //нет базы
+                emitter.onError(RuntimeException("Cache Chart is null"))
             }
             emitter.onComplete()
         }
     }
 
-    override fun putChartById(id: String, chart: GeckoCoinChartRest): Boolean{
+    override fun putChartById(id: String, chart: GeckoCoinChartRest): Boolean {
         try {
-            Paper.book(KEY_CHART).delete(id)
-            Paper.book(KEY_CHART).write(id,chart)
+            if (Paper.book(KEY_CHART).contains(id)) {
+                Paper.book(KEY_CHART).delete(id)
+            }
+            Paper.book(KEY_CHART).write(id, chart)
             return true
-        } catch (e: Exception){
+        } catch (e: Exception) {
             return false
         }
     }
